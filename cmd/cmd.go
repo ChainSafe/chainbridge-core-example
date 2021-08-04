11d01ca42ec5de307120b75493ff7f5ec078ba18
Kirill Pisariev <pisarevkir@gmail.com>
2021-08-04 18:50:57 +0300

    add CLI

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"github.com/ChainSafe/chainbridge-celo-module/cli"
 	"github.com/ChainSafe/chainbridge-core-example/example"
 	"github.com/ChainSafe/chainbridge-core/config"
 	"github.com/rs/zerolog/log"
@@ -12,6 +13,9 @@ import (
 
 var (
 	rootCMD = &cobra.Command{
+		Use: "",
+	}
+	runCMD = &cobra.Command{
 		Use:   "run",
 		Short: "Run example app",
 		Long:  "Run example app",
@@ -22,14 +26,19 @@ var (
 			return nil
 		},
 	}
+	cliCMD = &cobra.Command{
+		Use:   "cli",
+		Short: "cli",
+	}
 )
 
 func init() {
-	//rootCMD.AddCommand(evmClient.CLI()) // Example of how CLI should be registered
 	config.BindFlags(rootCMD)
+	cliCMD.AddCommand(cli.DeployCELO)
 }
 
 func Execute() {
+	rootCMD.AddCommand(runCMD, cliCMD)
 	if err := rootCMD.Execute(); err != nil {
 		log.Fatal().Err(err).Msg("failed to execute root cmd")
 	}
--- example/app.go
+++ example/app.go
@@ -8,7 +8,8 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/ChainSafe/chainbridge-core-example/example/keystore"
+	"github.com/ChainSafe/chainbridge-celo-module/transaction"
+
 	"github.com/ChainSafe/chainbridge-core/chains/evm"
 	"github.com/ChainSafe/chainbridge-core/chains/evm/evmclient"
 	"github.com/ChainSafe/chainbridge-core/chains/evm/listener"
@@ -21,23 +22,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-var AliceKp = keystore.TestKeyRing.EthereumKeys[keystore.AliceKey]
-var BobKp = keystore.TestKeyRing.EthereumKeys[keystore.BobKey]
-var EveKp = keystore.TestKeyRing.EthereumKeys[keystore.EveKey]
-
-var (
-	DefaultRelayerAddresses = []common.Address{
-		common.HexToAddress(keystore.TestKeyRing.EthereumKeys[keystore.AliceKey].Address()),
-		common.HexToAddress(keystore.TestKeyRing.EthereumKeys[keystore.BobKey].Address()),
-		common.HexToAddress(keystore.TestKeyRing.EthereumKeys[keystore.CharlieKey].Address()),
-		common.HexToAddress(keystore.TestKeyRing.EthereumKeys[keystore.DaveKey].Address()),
-		common.HexToAddress(keystore.TestKeyRing.EthereumKeys[keystore.EveKey].Address()),
-	}
-)
-
-
-//Bridge:             0x62877dDCd49aD22f5eDfc6ac108e9a4b5D2bD88B
-//Erc20 Handler:      0x3167776db165D8eA0f51790CA2bbf44Db5105ADF
 func Run() error {
 	errChn := make(chan error)
 	stopChn := make(chan struct{})
@@ -60,14 +44,11 @@ func Run() error {
 	celoListener1 := listener.NewEVMListener(celo1Client, eventHandler, common.HexToAddress(celo1Cfg.SharedEVMConfig.Bridge))
 	mh := voter.NewEVMMessageHandler(celo1Client, common.HexToAddress(celo1Cfg.SharedEVMConfig.Bridge))
 	mh.RegisterMessageHandler(common.HexToAddress(celo1Cfg.SharedEVMConfig.Erc20Handler), voter.ERC20MessageHandler)
-	celoVoter1 := voter.NewVoter(mh, celo1Client)
+	celoVoter1 := voter.NewVoter(mh, celo1Client, transaction.NewCeloTransaction)
 	celoChain1 := evm.NewEVMChain(celoListener1, celoVoter1, db, *celo1Cfg.SharedEVMConfig.GeneralChainConfig.Id, &celo1Cfg.SharedEVMConfig)
 
 	//// CELO2 setup
 	celoClient2 := evmclient.NewEVMClient()
-	if err != nil {
-		panic(err)
-	}
 	err = celoClient2.Configurate(viper.GetString(config.ConfigFlagName), "config_celo2.json")
 	if err != nil {
 		panic(err)
@@ -79,7 +60,7 @@ func Run() error {
 	goerliListener := listener.NewEVMListener(celoClient2, eventHandlerCelo2, common.HexToAddress(celoConfig2.SharedEVMConfig.Bridge))
 	mhCelo2 := voter.NewEVMMessageHandler(celoClient2, common.HexToAddress(celoConfig2.SharedEVMConfig.Bridge))
 	mhCelo2.RegisterMessageHandler(common.HexToAddress(celoConfig2.SharedEVMConfig.Erc20Handler), voter.ERC20MessageHandler)
-	celoVoter2 := voter.NewVoter(mhCelo2, celoClient2)
+	celoVoter2 := voter.NewVoter(mhCelo2, celoClient2, transaction.NewCeloTransaction)
 	celoChain2 := evm.NewEVMChain(goerliListener, celoVoter2, db, *celoConfig2.SharedEVMConfig.GeneralChainConfig.Id, &celoConfig2.SharedEVMConfig)
 
 	r := relayer.NewRelayer([]relayer.RelayedChain{celoChain1, celoChain2})
@@ -99,7 +80,7 @@ func Run() error {
 		close(stopChn)
 		return err
 	case sig := <-sysErr:
-		log.Info().Msgf("terminating got [%v] signal", sig)
+		log.Info().Msgf("terminating got ` [%v] signal", sig)
 		return nil
 	}
 }
