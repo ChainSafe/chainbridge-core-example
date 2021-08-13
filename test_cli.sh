018a58b41805818f6dab8b1a362095b1bedad1b6
waymobetta <jbroethke@gmail.com>
2021-08-12 19:17:31 -0700

    Replace test values to make generic

--- test_cli.sh
+++ test_cli.sh
@@ -4,51 +4,79 @@
 
 CMD=./chainbridge-core-example
 
-KALEIDO_NODE_URL="wss://u0oo16l2hl:WQgUMKVenv9mefKXW57fV-TzSWhFz06YILiXlHGykd4@u0ize6fsgo-u0np93mhu4-wss.us0-aws.kaleido.io"
-
-BRIDGE_ADDRESS="0xD83cA9C4EEa4B33F110d9F9e5E462D4111335090"
-ERC20_ADDRESS="0xa0a041d451c3ae2Cb32C0a8c9fD054D6bf281D02"
-ERC20_HANDLER="0x42e529d0249410fBf3b0690Ca2f100a83fB78F5F"
-RESOURCE_ID="000000000000000000000021605f71845f372A9ed84253d2D024B7B10999f400"
-
+# Default values
 GAS_LIMIT=6721975
 GAS_PRICE=20000000000
 
-BRIDGE2_ADDRESS=""
+################
+# USER CONFIGURATIONS
+################
+
+# Node WS endpoint
+NODE_URL=""
+
+# Address to receive transfers
+RECIPIENT_ADDRESS=""
+
+# Addresses of the relayers
+RELAYER_ADDRESS1=""
+RELAYER_ADDRESS2=""
+RELAYER_ADDRESS3=""
+
+################
+# OUTPUTS
+################
+
+# Deployed bridge contract
+BRIDGE_ADDRESS=""
+
+# Deployed ERC20 contracts
+ERC20_ADDRESS=""
+ERC20_HANDLER=""
+
+# ERC20 Resource ID
+ERC20_RESOURCE_ID=""
+
+# Deployed ERC721 contracts
 ERC721_HANDLER=""
-ERC721_RESOURCE_ID="0000000000000000000000d7E33e1bbf65dC001A0Eb1552613106CD7e40C3100"
 ERC721_CONTRACT=""
 
-GENERIC_HANDLER=""
-GENERIC_RESOURCE_ID="0000000000000000000000106C24dc2D480b5559C9E0e97bAaDf0750d9F0B800"
+# ERC721 Resource ID
+ERC721_RESOURCE_ID=""
 
-RECIPIENT_ADDRESS="0x0"
+# Deployed Generic contracts
+GENERIC_HANDLER=""
+GENERIC_RESOURCE_ID=""
 
 set -eux
 
 ## deploy
 # deploy bridge
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE deploy --bridge --url $KALEIDO_NODE_URL --relayerThreshold 1 --relayers="$RECIPIENT_ADDRESS,$RECIPIENT_ADDRESS,$RECIPIENT_ADDRESS"
+{
+    $CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE deploy --bridge --url $NODE_URL --relayerThreshold 1 --relayers="$RELAYER_ADDRESS1,$RELAYER_ADDRESS2,$RELAYER_ADDRESS3"
+} || {
+	exit
+}
 
 # deploy erc20
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE deploy --erc20 --url $KALEIDO_NODE_URL --erc20Symbol "TKN" --erc20Name "token"
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE deploy --erc20 --url $NODE_URL --erc20Symbol "TKN" --erc20Name "token"
 
 # deploy erc20 handler
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE deploy --erc20Handler --url $KALEIDO_NODE_URL --bridgeAddress $BRIDGE_ADDRESS
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE deploy --erc20Handler --url $NODE_URL --bridgeAddress $BRIDGE_ADDRESS
 
 ## bridge
 # register resource
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE bridge register-resource --url $KALEIDO_NODE_URL --resourceId $RESOURCE_ID --target $ERC20_HANDLER
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE bridge register-resource --url $NODE_URL --resourceId $ERC20_RESOURCE_ID --target $ERC20_HANDLER
 
 # register contract as mintable/burnable
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE bridge set-burn --url $KALEIDO_NODE_URL --bridge $BRIDGE_ADDRESS --handler $ERC20_HANDLER --tokenContract $ERC20_ADDRESS
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE bridge set-burn --url $NODE_URL --bridge $BRIDGE_ADDRESS --handler $ERC20_HANDLER --tokenContract $ERC20_ADDRESS
 
 ## erc20
 # register handler as mintable
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE erc20 add-minter --url $KALEIDO_NODE_URL --minter $ERC20_HANDLER --erc20Address $ERC20_ADDRESS
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE erc20 add-minter --url $NODE_URL --minter $ERC20_HANDLER --erc20Address $ERC20_ADDRESS
 
 # approve erc20
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE erc20 approve --url $KALEIDO_NODE_URL --erc20Address $ERC20_ADDRESS --recipient $RECIPIENT_ADDRESS --amount "1.11"  --decimals 2
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE erc20 approve --url $NODE_URL --erc20Address $ERC20_ADDRESS --recipient $RECIPIENT_ADDRESS --amount "1.11"  --decimals 2
 
 # mint erc20
-$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE erc20 mint --url $KALEIDO_NODE_URL --amount "100" --erc20Address $ERC20_ADDRESS
+$CMD evm-cli --gasLimit $GAS_LIMIT --gasPrice $GAS_PRICE erc20 mint --url $NODE_URL --amount "100" --erc20Address $ERC20_ADDRESS
