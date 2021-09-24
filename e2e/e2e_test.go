d14c9f8b89c0be89baf61215f355fef3ba0426c8
Kirill Pisariev <pisarevkir@gmail.com>
2021-09-24 10:51:40 +0300

    small updates

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -18,13 +18,5 @@ const CeloEndpoint = "http://localhost:8546"
 
 // Alice key is used by the relayer, Eve key is used as admin and depositter
 func TestRunE2ETests(t *testing.T) {
-	//c, err := evmclient.NewEVMClientFromParams(CeloEndpoint, evm.AliceKp.PrivateKey(), big.NewInt(20000000000))
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
-	//_, err = c.Simulate(big.NewInt(200), common.HexToHash("0x7025762ad07d80ff6eb560d47e6be146331e5ae66563dd772e2423c16ec09698"), evm.AliceKp.CommonAddress())
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
 	suite.Run(t, evm.PreSetupTestSuite(evmtransaction.NewTransaction, transaction.NewCeloTransaction, ETHEndpoint, CeloEndpoint, evm.EveKp))
 }
