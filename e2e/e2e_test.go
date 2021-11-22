ec7f78b1e110d44204a2c5137fc61b7dd5183c68
Nikola <nikola.mlinaric93@gmail.com>
2021-11-22 13:51:29 +0100

    Update e2e tests

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -10,13 +10,16 @@ import (
 	"github.com/ChainSafe/chainbridge-celo-module/transaction"
 	"github.com/ChainSafe/chainbridge-core/chains/evm/evmtransaction"
 	"github.com/ChainSafe/chainbridge-core/e2e/evm"
+	"github.com/ChainSafe/chainbridge-core/keystore"
 	"github.com/stretchr/testify/suite"
 )
 
 const ETHEndpoint = "http://localhost:8845"
 const CeloEndpoint = "http://localhost:8546"
 
+var EveKp = keystore.TestKeyRing.EthereumKeys[keystore.EveKey]
+
 // Alice key is used by the relayer, Eve key is used as admin and depositter
 func TestRunE2ETests(t *testing.T) {
-	suite.Run(t, evm.PreSetupTestSuite(evmtransaction.NewTransaction, transaction.NewCeloTransaction, ETHEndpoint, CeloEndpoint, evm.EveKp))
+	suite.Run(t, evm.SetupEVM2EVMTestSuite(evmtransaction.NewTransaction, transaction.NewCeloTransaction, ETHEndpoint, CeloEndpoint, EveKp))
 }
